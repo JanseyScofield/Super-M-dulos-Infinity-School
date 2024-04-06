319a6d25b01fd3db8b098007d78d19d5d8628127
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-04-06 09:07:34 -0300

    Aula 1.2-Mickey.py

--- Python Art Criative/Aula 1.2-Mickey.py
+++ Python Art Criative/Aula 1.2-Mickey.py
@@ -1,10 +1,11 @@
-#Des
-
 def setup():
     size(960, 960)
+    background(255,255,255)
     mickey()
 def mickey(x,y,c):
     no_stroke()
+    fill(255,0,0)
     circle(x,y,c)
+    fill(0,0,0)
     circle(x-(c/2),y-(c/2),c/2)
     circle(x+(c/2),y-(c/2),c/2)
